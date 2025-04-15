8e005bd8b9ff2339e916102d92843b85aea34ab1
francescocifaldi <francesco.cifaldi@gmail.com>
2025-04-15 14:58:31 +0200

    first commit

---
+++ src/org/Studente.java
@@ -0,0 +1,20 @@
+package org;
+
+public class Studente {
+    // attributi
+    private String nome;
+    private String cognome;
+    private int eta;
+
+    // costruttore
+    public Studente(String nome, String cognome, int eta) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+    }
+
+    // metodi
+    public String getFullName() {
+        return this.nome + " " + this.cognome + ", " + this.eta + " anni";
+    }
+}
